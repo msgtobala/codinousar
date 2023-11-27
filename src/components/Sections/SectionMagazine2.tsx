2784d667f0fbb52dece724c1c7cb7d58da361d2e
Balaji Venkatraman <balaji@moonraft.com>
2023-11-27 16:32:29 +0530

    fix magazine 6 order

--- src/components/Sections/SectionMagazine2.tsx
+++ src/components/Sections/SectionMagazine2.tsx
@@ -1,19 +1,27 @@
-import React, { FC } from "react";
-import Card2 from "@/components/Card2/Card2";
-import { SectionMagazine1Props } from "./SectionMagazine1";
-import Card11 from "@/components/Card11/Card11";
-import Empty from "../Empty";
+import React, { FC } from 'react';
+import Card2 from '@/components/Card2/Card2';
+import { SectionMagazine1Props } from './SectionMagazine1';
+import Card11 from '@/components/Card11/Card11';
+import Empty from '../Empty';
+import { useWindowSize } from 'react-use';
 
 export interface SectionMagazine2Props extends SectionMagazine1Props {}
 
 const SectionMagazine2: FC<SectionMagazine2Props> = ({ posts, className }) => {
+  const { width } = useWindowSize();
+  const isMobile = width < 640;
+
   return (
     <div className={`nc-SectionMagazine2 ${className}`}>
       {!posts.length ? (
         <Empty />
       ) : (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div
+            className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ${
+              isMobile ? 'order-1' : ''
+            }`}
+          >
             <div className="grid gap-6">
               {posts
                 .filter((_, i) => i < 3 && i > 0)
@@ -27,7 +35,7 @@ const SectionMagazine2: FC<SectionMagazine2Props> = ({ posts, className }) => {
                   );
                 })}
             </div>
-            <div className="lg:col-span-2">
+            <div className={`lg:col-span-2 ${isMobile ? 'order-0' : ''}`}>
               {posts[0] && <Card2 size="large" post={posts[0]} />}
             </div>
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-1 md:col-span-3 xl:col-span-1">
@@ -46,7 +54,11 @@ const SectionMagazine2: FC<SectionMagazine2Props> = ({ posts, className }) => {
           </div>
 
           {!!posts[5] && (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
+            <div
+              className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6 ${
+                isMobile ? 'order-2' : ''
+              }`}
+            >
               {posts
                 .filter((_, i) => i >= 5)
                 .map((item) => {
